b3fbc15ffa8bb154b5bfc4ae27bd3b23df0c9363
luminous <2815792820@qq.com>
2023-08-09 19:43:17 +0800

    新增user和video的proto文件

--- app/gateway/router/router.go
+++ app/gateway/router/router.go
@@ -1,8 +1,8 @@
 package router
 
 import (
-	"douyin-microservice/app/gateway/middleware"
 	"douyin-microservice/app/gateway/http"
+	"douyin-microservice/app/gateway/middleware"
 	"github.com/gin-gonic/gin"
 )
 
@@ -11,32 +11,32 @@ func InitRouter() *gin.Engine {
 	r.Use(middleware.Cors())
 	// public directory is used to serve static resources
 	r.Static("/static", "./public")
-
-	apiRouter1 := r.Group("/douyin")
-	apiRouter1.Use(middleware.RefreshHandler())
+	//所有请求都需要两层拦截器
+	apiRouter := r.Group("/douyin")
+	apiRouter.Use(middleware.RefreshHandler())
 	// basic    apis
-	apiRouter1.GET("/feed/", http.FeedHandler)
-	apiRouter1.POST("/user/register/", http.RegisterHandler)
-	apiRouter1.POST("/user/login/", http.LoginHandler)
+	apiRouter.GET("/feed/", http.FeedHandler)
+	apiRouter.POST("/user/register/", http.RegisterHandler)
+	apiRouter.POST("/user/login/", http.LoginHandler)
 
-	apiRouter2 := r.Group("/douyin"
-	apiRouter2.Use(middleware.AuthAdminCheck())
+	//apiRouter2 := r.Group("/douyin")
+	apiRouter.Use(middleware.AuthAdminCheck())
 	// extra apis - I
-	apiRouter2.POST("/favorite/action/", http.FavoriteActionHandler)
-	apiRouter2.GET("/favorite/list/", http.FavoriteListHandler)
-	apiRouter2.POST("/comment/action/", http.CommentActionHandler)
-	apiRouter2.GET("/comment/list/", http.CommentListHandler)
+	apiRouter.POST("/favorite/action/", http.FavoriteActionHandler)
+	apiRouter.GET("/favorite/list/", http.FavoriteListHandler)
+	apiRouter.POST("/comment/action/", http.CommentActionHandler)
+	apiRouter.GET("/comment/list/", http.CommentListHandler)
 
 	// extra apis - II
-	apiRouter2.POST("/relation/action/", http.RelationActionHandler)
-	apiRouter2.GET("/relation/follow/list/", http.FollowListHandler)
-	apiRouter2.GET("/relation/follower/list/", http.FollowerListHandler)
-	apiRouter2.GET("/relation/friend/list/", http.FriendListHandler)
-	apiRouter2.GET("/message/chat/", http.MessageChatHandler)
-	apiRouter2.POST("/message/action/", http.MessageActionHandler)
+	apiRouter.POST("/relation/action/", http.RelationActionHandler)
+	apiRouter.GET("/relation/follow/list/", http.FollowListHandler)
+	apiRouter.GET("/relation/follower/list/", http.FollowerListHandler)
+	apiRouter.GET("/relation/friend/list/", http.FriendListHandler)
+	apiRouter.GET("/message/chat/", http.MessageChatHandler)
+	apiRouter.POST("/message/action/", http.MessageActionHandler)
 
-	apiRouter2.GET("/user/", http.UserInfoHandler)
-	apiRouter2.POST("/publish/action/", http.PublishHandler)
-	apiRouter2.GET("/publish/list/", http.PublishListHandler)
+	apiRouter.GET("/user/", http.UserInfoHandler)
+	apiRouter.POST("/publish/action/", http.PublishHandler)
+	apiRouter.GET("/publish/list/", http.PublishListHandler)
 
 }
